749ba4e7360b3b5582362f1a1570896e9cde9d83
Shubham Singh <shubhammahar1306@gmail.com>
2024-12-30 10:55:35 +0530

    program to sort an array

---
+++ sort-an-array.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+     
+        int[] arr = {5, 3, 8, 1};
+
+        Arrays.sort(arr);
+        
+        for (int i : arr) {
+            System.out.println(i);
+        }
+    }
+}
