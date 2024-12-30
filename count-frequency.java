f8297abe2824494f61bdae205497226078cf93a1
Shubham Singh <shubhammahar1306@gmail.com>
2024-12-30 11:48:37 +0530

    program to count the frequency of elements in array

--- count-frequency.java
+++ count-frequency.java
@@ -2,13 +2,16 @@ import java.util.*;
 class Main {
     public static void main(String[] args) {
      
-        int arr[] = {1,1,2,2,2,3,3,3,3,4,4,5,5,6,6,7};
-
+        int[] arr = {1, 2, 2, 3, 4, 4, 5};
+        
         HashMap<Integer, Integer> hm = new HashMap<>();
-        for (int i: arr){
+        
+        for (int i: arr) {
             hm.put(i, hm.getOrDefault(i,0)+1);
         }
         
-        System.out.println(hm);
+        for (Integer s: hm.keySet()) {
+            System.out.println("Key: " + s + ", Value: " + hm.get(s));
+        }
     }
 }
