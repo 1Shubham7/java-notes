dc72e4314fbb3b7e526c2375f6ed9e35b8889acf
Shubham Singh <shubhammahar1306@gmail.com>
2024-12-30 11:15:16 +0530

    program to reverse an array

--- reverse-an-array.java
+++ reverse-an-array.java
@@ -1,3 +1,26 @@
+// two pointer method
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+     
+        int arr[] = {1,1,2,2,2,3,3,3,3,4,4,5,5,6,6,7};
+
+        int i = 0;
+        int j = arr.length -1;
+        while (i < j){
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+            j--;
+        }
+        
+        for (int k : arr) {
+            System.out.println(k);
+        }
+    }
+}
+
 import java.util.*;
 class Main {
     public static void main(String[] args) {
